7cf1dd0e04f204ccd7df58b68441091fc8fd9b1b
kacper <kacper.nyk@onet.pl>
2022-06-12 21:10:50 +0200

    project done

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,19 @@ import * as fku from "./getFakeUsers.js";
 
 const form = document.getElementById("form");
 const errMsgCon = document.getElementById("errorMsgCon");
+let errValidateArr = [];
 
-function validateIfInputsAreEmpty() {
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  validateInputs();
+});
+
+function validateInputs() {
+  const passwordValue = fku.password.value;
+  const passwordValidateValue = fku.passwordValidation.value;
+  fku.emailInp.preventDefault();
+  const emailValue = fku.emailInp.value;
+  //check for all inputs if any is empty
   const allInputsElements = [...document.querySelectorAll("input")];
   allInputsElements.forEach((input) => {
     if (input.value === "") {
@@ -15,33 +26,65 @@ function validateIfInputsAreEmpty() {
       }
     }
   });
-}
-
-form.addEventListener("submit", (e) => {
-  validateIfInputsAreEmpty();
-  const passwordValue = fku.password.value;
-  const passwordValidateValue = fku.passwordValidation.value;
-  const loginValue = fku.loginInp.value;
-  const firstNameValue = fku.firstNameInp.value;
-  const lastNameValue = fku.lastNameInp.value;
-  const emailValue = fku.emailInp.value;
-  const phoneValue = fku.phoneInp.value;
-  const birthdayValue = fku.birthdayInp.value;
-  const countryValue = fku.countryInp.value;
-  const cityValue = fku.cityInp.value;
-  const cityCodeValue = fku.cityCodeInp.value;
-
-  let errValidateArr = [];
+  //validate password
   if (passwordValue.length < 6 || passwordValue.lenght > 25) {
     fku.password.classList.add("err");
     errValidateArr.push("wrong password");
-    console.log(errValidateArr);
+  }
+  if (!(passwordValue === passwordValidateValue)) {
+    errValidateArr.push("passwords doesn't match");
+    console.log("kasld");
   }
   if (errValidateArr.length > 0) {
     errMsgCon.innerText = errValidateArr;
     errMsgCon.classList.add("active");
     setTimeout(() => {
       errMsgCon.classList.remove("active");
-    }, 1500);
+      errValidateArr = [];
+    }, 2000);
   }
-});
+  //email
+  if (!isEmail(emailValue)) {
+    fku.emailInp.value = `"${emailValue} " is not correct email`;
+    fku.emailInp.classList.add("err");
+  }
+  //age
+  ValidateAge();
+}
+
+
+function isEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+}
+
+function ValidateAge() {
+  const birthdayValue = fku.birthdayInp.value;
+  const regex = /(((0|1)[0-9]|2[0-9]|3[0-1])\/(0[1-9]|1[0-2])\/((19|20)\d\d))$/;
+
+  if (regex.test(birthdayValue)) {
+    const parts = birthdayValue.split("/");
+    const dtDOB = new Date(parts[1] + "/" + parts[0] + "/" + parts[2]);
+    const dtCurrent = new Date();
+    if (dtCurrent.getFullYear() - dtDOB.getFullYear() < 18) {
+      fku.birthdayInp.value = "You must be 18 years old";
+      return false;
+    }
+    if (dtCurrent.getFullYear() - dtDOB.getFullYear() == 18) {
+      if (dtCurrent.getMonth() < dtDOB.getMonth()) {
+        return false;
+      }
+      if (dtCurrent.getMonth() == dtDOB.getMonth()) {
+        if (dtCurrent.getDate() < dtDOB.getDate()) {
+          return false;
+        }
+      }
+    }
+    return true;
+  } else {
+    fku.birthdayInp.value = "enter like this DD/MM/YYYY";
+    fku.birthdayInp.classList.add("err")
+    return false;
+  }
+}
